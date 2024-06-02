5368f10caee92d11df9c52f29734a4ad5c9ae133
LDJ <eowls5602@gmail.com>
2024-06-02 15:54:55 +0900

    days 3

---
+++ algorithms-in-JS/50days/Days1_Days10/Days_3/d3.js
@@ -0,0 +1,15 @@
+function solution(angle) {
+    var answer = 0;
+    if(angle < 90){
+      return 1;
+    }
+    else if(angle == 90){
+      return 2;
+    }
+    else if(angle < 180){
+      return 3;
+    }
+    else {
+      return 4;
+    }
+}
